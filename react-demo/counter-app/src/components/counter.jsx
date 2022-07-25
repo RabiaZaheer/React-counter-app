9e23f294603b170a3de6e81728d135f7b7e24129
Rabia Zaheer <rabia.majid@live.com>
2022-07-25 09:48:22 +0930

    2nd Commit

--- react-demo/counter-app/src/components/counter.jsx
+++ react-demo/counter-app/src/components/counter.jsx
@@ -2,42 +2,38 @@ import React, {Component} from "react";
 
 class Counter extends Component {
 
- state ={ 
-    count:0,
-    tags:['tag1','tag2','tag3']
-    
-    
- };
- handleIncrement(){
-    console.log("increment Clicked");
- }
- style={
-    fontSize:'10px',
-    fontWeight: "bold"
- };
 
-    render() {     
+ 
+ 
+    render() {  
       
-        return (
-        <React.Fragment> 
-            
-            <span style={this.style } className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm" onClick={this.handleIncrement()}>Increment</button>
-            <ul>
-              {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-            </ul>
-        </React.Fragment>
+      return (
+      <div>
+             
+            <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button 
+            className="btn btn-secondary btn-sm" onClick={()=> this.props.onIncrement(this.props.counter)}>
+               Increment
+               </button>
+        
+               <button onClick={()=>this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2" >
+              Delete
+               </button>
+
+
+
+        </div>
    );
  }
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
  formatCount(){
-    const { count} = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value} = this.props.counter;
+    return value === 0 ? "Zero" : value;
  }
 }
 
--- react-demo/counter-app/src/index.js
+++ react-demo/counter-app/src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import "bootstrap/dist/css/bootstrap.css";
+
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>
 );
 
